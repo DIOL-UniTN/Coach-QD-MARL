ec0955e28facb96e7422fdc5206879b2764a110c
NielsenErik <eriknielsen98@gmail.com>
2024-05-09 11:15:26 +0200

    merged from private repo

--- src/base/dts4marl/decisiontreelibrary/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from .leaves import QLearningLeafFactory
-from .nodes import *
-from .conditions import ConditionFactory
-from .trees import DecisionTree, RLDecisionTree
-
-# MIA AGGIUNTA
-from .conditions import Condition
-from .leaves import Leaf
--- src/base/dts4marl/launch.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-python3 src/base/dts4marl/launcher.py src/base/dts4marl/battlefield.json 4
